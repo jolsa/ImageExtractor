8824ae33b2a764637e75c29215185d353fef541c
Johnny Olsa <johnnyiv@pobox.com>
2021-06-27 17:27:01 -0500

    - Added fonts (glyphs) - Added URL list - Reformat javascript

--- content/Images.js
+++ content/Images.js
@@ -1,42 +1,47 @@
-﻿"use strict";
+﻿'use strict';
 
 var rendered, items = [];
-function renderItems(passedItems)
-{
+function renderItems(passedItems) {
 	if (rendered) return;
 	items = passedItems;
 	rendered = true;
 }
 
-(function ()
-{
+var elementId = 'urlText';
 
-	var name = "images";
-	angular.module(name, ["settings"])
-		.controller(name, ["$scope", "settings", function ($scope, settingsFactory)
-		{
+function copyToClipboard(elementId) {
+	var range = document.createRange();
+	range.selectNode(document.getElementById(elementId));
+	window.getSelection().removeAllRanges(); // clear current selection
+	window.getSelection().addRange(range); // to select text
+	document.execCommand("copy");
+	window.getSelection().removeAllRanges(); // to deselect
+}
+
+(function () {
+
+	var name = 'images';
+	angular.module(name, ['settings'])
+		.controller(name, ['$scope', '$timeout', 'settings', function ($scope, $timeout, settingsFactory) {
 			var naming = $scope.naming = settingsFactory.naming;
-			var nameTypes = { sequenced: "Use Sequenced Names", deflt: "Use Default Names" };
+			var nameTypes = { sequenced: 'Use Sequenced Names', deflt: 'Use Default Names' };
 			$scope.defaultNames = false;
 			$scope.nameType = nameTypes.deflt;
 			$scope.imageCount = 0;
 			$scope.borders = false;
 			$scope.canShow = false;
-			$scope.imgClass = "stacked";
-			$scope.sortBy = "area";
+			$scope.imgClass = 'stacked';
+			$scope.sortBy = 'area';
 			$scope.data = [];
 
-			$scope.apply = function ()
-			{
-				if ($scope.$root.$$phase !== "$apply" && $scope.$root.$$phase !== "$digest")
+			$scope.apply = function () {
+				if ($scope.$root.$$phase !== '$apply' && $scope.$root.$$phase !== '$digest')
 					$scope.$apply();
 			};
 
 			//	Wait until "rendered" is set, then set data to items
-			(function wait()
-			{
-				if (!rendered)
-				{
+			(function wait() {
+				if (!rendered) {
 					setTimeout(wait, 1);
 					return;
 				}
@@ -47,26 +52,23 @@ function renderItems(passedItems)
 				chrome.downloads.onDeterminingFilename.addListener(naming.renamer);
 				chrome.downloads.onCreated.addListener(function () { setTimeout($scope.apply, 10); });
 			})();
-			$scope.toggleNames = function()
-			{
+			$scope.toggleNames = function () {
 				var b = $scope.defaultNames = !$scope.defaultNames;
 				$scope.nameType = b ? nameTypes.sequenced : nameTypes.deflt;
 				settingsFactory.naming.useDefault = b;
 			}
-			$scope.sort = function (sortBy)
-			{
+			$scope.sort = function (sortBy) {
 				$scope.sortBy = sortBy;
 				$scope.data					//	Sort by size
-					.sort(function (a, b)
-					{
+					.sort(function (a, b) {
 						var r;
-						if (sortBy === "area")
+						if (sortBy === 'area')
 							r = b.w * b.h - a.w * a.h;
-						else if (sortBy === "width")
+						else if (sortBy === 'width')
 							r = b.w - a.w;
-						else if (sortBy === "height")
+						else if (sortBy === 'height')
 							r = b.h - a.h;
-						else if (sortBy === "index")
+						else if (sortBy === 'index')
 							r = 0;
 						return r || a.index - b.index;
 					})
@@ -74,39 +76,48 @@ function renderItems(passedItems)
 					.forEach(function (e, ord) { e.ord = ord; });
 
 			};
-			$scope.hideImage = function ()
-			{
+			$scope.hideImage = function () {
 				$scope.imageCount--;
 				$scope.canShow = true;
 			};
-			$scope.showAll = function ()
-			{
+			$scope.showAll = function () {
 				$scope.imageCount = items.length;
 				$scope.canShow = false;
 				$scope.data.forEach(function (e) { e.hide = false; })
 			};
-			$scope.toggleBorders = function()
-			{
+			$scope.toggleBorders = function () {
 				$scope.showBorders = !$scope.showBorders;
 				if ($scope.showBorders)
-					$scope.imgClass += " imgBorder";
+					$scope.imgClass += ' imgBorder';
 				else
-					$scope.imgClass = $scope.imgClass.replace(/ imgBorder/i, "");
+					$scope.imgClass = $scope.imgClass.replace(/ imgBorder/i, '');
 			}
-			$scope.saveAll = function ()
-			{
+			$scope.saveAll = function () {
 				//	Reset the ordinal and timestamp
 				naming.reset();
-				$scope.data.forEach(function (e)
-				{
+				$scope.data.forEach(function (e) {
 					if (!e.hide)
 						chrome.downloads.download({ url: e.img, saveAs: false });
 				});
 			};
-			$scope.resetNames = function()
-			{
+			$scope.resetNames = function () {
 				naming.clear();
-			}
+			};
+			$scope.showUrls = function () {
+				$scope.urls = $scope.data.filter(function (i) { return !i.hide; }).map(function (i) { return i.img; }).join('\r\n');
+				$scope.urlsVisible = true;
+			};
+			$scope.copyUrl = function () {
+				$scope.timedText = true;
+				copyToClipboard(elementId);
+				$timeout(function () { data.timedText = false; }, 2000);
+			};
+			$(document).keydown(function (e) {
+				if (e.which === 27) {
+					$scope.urlsVisible = false;
+					$scope.apply();
+				}
+			});
 		}]);
 
 })();
--- popup.js
+++ popup.js
@@ -1,11 +1,19 @@
-(function ()
-{
-	"use strict";
+(function () {
+	'use strict';
 
-	var imageDiv = $("#images");
+	var imageDiv = jQuery('#images');
+	var elementId = 'urlText';
 
-	function controller($scope, $filter, settingsFactory)
-	{
+	function copyToClipboard(elementId) {
+		var range = document.createRange();
+		range.selectNode(document.getElementById(elementId));
+		window.getSelection().removeAllRanges(); // clear current selection
+		window.getSelection().addRange(range); // to select text
+		document.execCommand("copy");
+		window.getSelection().removeAllRanges(); // to deselect
+	}
+
+	function controller($scope, $timeout, settingsFactory) {
 		var settings = settingsFactory.settings;
 		var naming = $scope.naming = settingsFactory.naming;
 
@@ -13,114 +21,100 @@
 		$scope.data = data;
 
 		//	Using this prevents $apply from throwing an error
-		$scope.apply = function ()
-		{
-			if ($scope.$root.$$phase !== "$apply" && $scope.$root.$$phase !== "$digest")
+		$scope.apply = function () {
+			if ($scope.$root.$$phase !== '$apply' && $scope.$root.$$phase !== '$digest')
 				$scope.$apply();
-			};
+		};
 		chrome.downloads.onDeterminingFilename.addListener(naming.renamer);
 		chrome.downloads.onCreated.addListener(function () { setTimeout($scope.apply, 10); });
-		$scope.getOpenAllTitle = function ()
-		{
-			return $scope.canOpenAll() ? "Open each image in a new tab" : "Must be " + data.maxOpen + " or less images shown to Open All.";
+		$scope.getOpenAllTitle = function () {
+			return $scope.canOpenAll() ? 'Open each image in a new tab' : 'Must be ' + data.maxOpen + ' or less images shown to Open All.';
 		};
-		$scope.canOpenAll = function ()
-		{
+		$scope.canOpenAll = function () {
 			return data.imageCount && data.imageCount <= data.maxOpen;
 		};
-		$scope.showError = function (message)
-		{
+		$scope.showError = function (message) {
 			data.dangerMessage = message;
 			$scope.apply();
 			//	Clear message after timeout
-			setTimeout(function ()
-			{
+			setTimeout(function () {
 				data.dangerMessage = null;
 				$scope.apply();
 			}, settings.errMsgTimeout);
 		};
-		$scope.openImage = function (item)
-		{
+		$scope.openImage = function (item) {
 			chrome.tabs.create({ url: item.img.img, active: false });
 		};
-		$scope.openAll = function ()
-		{
+		$scope.openAll = function () {
 			//	Don't "open all" if there are more than maxOpen
 			if (!data.imageCount || data.imageCount > data.maxOpen) return;
-			data.imgs.forEach(function (e)
-			{
+			data.imgs.forEach(function (e) {
 				if (!e.hide)
 					chrome.tabs.create({ url: e.img, active: false });
 			});
 		};
-		$scope.saveAll = function ()
-		{
+		$scope.saveAll = function () {
 			//	Reset the ordinal and timestamp
 			naming.reset();
-			data.imgs.forEach(function (e)
-			{
+			data.imgs.forEach(function (e) {
 				if (!e.hide)
 					chrome.downloads.download({ url: e.img, saveAs: false });
 			});
 		};
-		$scope.toNewPage = function()
-		{
-			chrome.tabs.create({ url: chrome.extension.getURL("content/Images.html"), active: false }, function (tab)
-			{
+		$scope.toNewPage = function () {
+			chrome.tabs.create({ url: chrome.extension.getURL('content/Images.html'), active: false }, function (tab) {
 				var selfTabId = tab.id;
-				chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab)
-				{
-					if (changeInfo.status === "complete" && tabId === selfTabId)
-					{
-						var tabs = chrome.extension.getViews({ type: "tab" });
+				chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+					if (changeInfo.status === 'complete' && tabId === selfTabId) {
+						var tabs = chrome.extension.getViews({ type: 'tab' });
 						tabs[0].renderItems(data.imgs);
 						chrome.tabs.update(tabId, { active: true });
 					}
 				});
 			});
-		},
-		$scope.hideImage = function ()
-		{
+		};
+		$scope.showUrls = function () {
+			data.urls = data.imgs.filter(function (i) { return !i.hide; }).map(function (i) { return i.img; }).join('\r\n');
+			data.urlsVisible = true;
+		};
+		$scope.copyUrl = function () {
+			data.timedText = true;
+			copyToClipboard(elementId);
+			$timeout(function () { data.timedText = false; }, 2000);
+		};
+		$scope.hideImage = function () {
 			//	Doing this from ng-Click doesn't seem to work
 			data.imageCount--;
 		};
-		$scope.loadImages = function ()
-		{
+		$scope.loadImages = function () {
 			naming.clear();
-			chrome.tabs.query({ active: true, currentWindow: true }, function (tabs)
-			{
+			chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
 				var id = tabs[0].id;
 				//	Inject jQuery first
-				chrome.tabs.executeScript(id, { file: "scripts/jquery-1.10.2.min.js" }, function ()
-				{
+				chrome.tabs.executeScript(id, { file: 'scripts/jquery-1.10.2.min.js' }, function () {
 					//	If we can't inject a script, show the message
-					if (chrome.runtime.lastError)
-					{
+					if (chrome.runtime.lastError) {
 						$scope.showError(chrome.runtime.lastError.message);
 						return;
 					}
 					data.loading = true;
-					chrome.tabs.executeScript(id, { file: "getImages.js" }, showImages);
+					chrome.tabs.executeScript(id, { file: 'getImages.js' }, showImages);
 				});
 			});
 		};
-		$scope.resetNames = function ()
-		{
+		$scope.resetNames = function () {
 			naming.clear();
 		}
-		function showImages(response)
-		{
+		function showImages(response) {
 
 			var info = response[0];
 			var loc = info.location;
 			var images = info.imgs;
 
 			//	Get distinct list
-			(function ()
-			{
+			(function () {
 				var a = [];
-				images.forEach(function (e)
-				{
+				images.forEach(function (e) {
 					var key = e.img.toLowerCase();
 					if (!a[key])
 						a[key] = e;
@@ -134,12 +128,10 @@
 			data.imgs = images;
 			$scope.apply();
 
-			var imgs = $("img", imageDiv).get();
+			var imgs = $('img', imageDiv).get();
 			//	Wait for images to load
-			(function wait()
-			{
-				if (data.imageCount = imgs.filter(function (e) { return !e.complete; }).length)
-				{
+			(function wait() {
+				if (data.imageCount = imgs.filter(function (e) { return !e.complete; }).length) {
 					//	Show progress and wait for 100 ms
 					$scope.apply();
 					setTimeout(wait, 100);
@@ -147,8 +139,7 @@
 				}
 
 				//	Get dimensions
-				images.forEach(function (e, i)
-				{
+				images.forEach(function (e, i) {
 					var img = imgs[i];
 					e.w = img.width;
 					e.h = img.height;
@@ -156,8 +147,7 @@
 				});
 				images
 					//	Sort by size
-					.sort(function (a, b)
-					{
+					.sort(function (a, b) {
 						var area = b.w * b.h - a.w * a.h;
 						return area || a.index - b.index;
 					})
@@ -176,7 +166,7 @@
 		$scope.loadImages();
 	}
 
-	var name = "extractor";
-	angular.module(name, ["settings"]).controller(name, ["$scope", "$filter", "settings", controller]);
+	var name = 'extractor';
+	angular.module(name, ['settings']).controller(name, ['$scope', '$timeout', 'settings', controller]);
 
 })();
